41bc1e169e4c7aaf34b766f3c1edb71d7f805416
Kiri4of <kopak7890@gmail.com>
2023-10-08 01:28:29 +0200

    first commit

---
+++ WeatherAppTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppTask
+//
+//  Created by Kiri4of on 08.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
